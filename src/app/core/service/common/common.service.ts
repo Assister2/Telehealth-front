47587294c4aefefbf0dd848bcae87e714406a5a4
Assister2 <staniwitkow@gmail.com>
2023-07-01 01:33:21 +1000

    Fix some errors

--- src/app/core/service/common/common.service.ts
+++ src/app/core/service/common/common.service.ts
@@ -55,7 +55,7 @@ export class CommonService {
   ];
 
   messages:any = '';
-  SERVER_URL: string = 'http://localhost:8100/api/v1/';
+  SERVER_URL: string = 'http://localhost:3000/api/v1/';
   public message: BehaviorSubject<String> = new BehaviorSubject<String>('');
   constructor(public http: HttpClient) {
     this.message = new BehaviorSubject(this.messages);
